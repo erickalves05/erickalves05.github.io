cb06ccc6b8a47ffa242aca57af76e317dfeb6651
Erick Alves <erickalves@outlook.com>
2015-08-14 21:37:32 -0300

    Update assets

---
+++ assets/js/area-admin.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="vendor/jquery-1.11.2.min.js" />
+
+$(document).ready(function () {
+    PosicionaFooter();
+
+    $(window).resize(function () {
+        PosicionaFooter();
+    });
+});
+
+
+function PosicionaFooter() {
+    if ($('footer').length) {
+        var docHeight = $(window).height();
+        var footerHeight = $('footer').height();
+        var footerTop = $('footer').position().top + footerHeight + 5;
+
+        if (footerTop < docHeight) {
+            $('footer').css('margin-top', (docHeight - footerTop) + 'px');
+        }
+    }
+}
---
+++ assets/js/site.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    if ($('.slider').length) {
+        $('.slider').bxSlider({
+            controls: false
+        });
+    }
+
+    if ($('.video-slider').length) {
+        $('.video-slider').bxSlider({
+            pager: false,
+            minSlides: 3,
+            maxSlides: 3,
+            slideWidth:280
+        });
+    }
+
+    PosicionaFooter();
+
+    $(window).resize(function () {
+        PosicionaFooter();
+    });
+});
+
+function PosicionaFooter() {
+    var docHeight = $(window).height();
+    var footerHeight = $('footer').height();
+    var footerTop = $('footer').position().top + footerHeight + 5;
+
+    if (footerTop < docHeight) {
+        $('footer').css('margin-top', (docHeight - footerTop) + 'px');
+    }
+}
